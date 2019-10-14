6d9cdb3cd6ae4294d5375f7cfbe7dfdb090a8208
XFNeo <xf.neo@mail.ru>
2019-10-14 19:56:49 +0400

    stylization with Vuetify + Material design icons

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -2,14 +2,18 @@ import Vue from 'vue'
 import VueResource from 'vue-resource'
 import App from 'pages/App.vue'
 import {connect} from 'util/ws.js'
+import Vuetify from "vuetify";
+import 'vuetify/dist/vuetify.min.css'
 
 if (frontendData.profile) {
     connect()
 }
 
+Vue.use(Vuetify)
 Vue.use(VueResource)
 
 new Vue({
+    vuetify: new Vuetify(),
     el: '#app',
     render: a=> a(App)
 })
